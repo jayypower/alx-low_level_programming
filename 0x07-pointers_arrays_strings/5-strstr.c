3507d3de1a7a78e8cde29f9071a6dc8d0cc11e0c
Jayypower <godsontariemi@gmail.com>
2022-10-31 10:12:20 -0700

    task 5 new

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,7 @@
  *
  * Return: If the substring is located - a pointer to the beginning
  * of the located substring.
- *  If the substring is not located - NULL.
+ * If the substring is not located - NULL.
  */
 
 char *_strstr(char *haystack, char *needle)
